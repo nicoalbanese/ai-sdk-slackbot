c488c372cf4897263176ddeec827b7d718623ba2
Nico Albanese <nico.albanese@vercel.com>
2025-03-03 16:47:24 +0000

    init starter branch

--- api/events.ts
+++ api/events.ts
@@ -1,10 +1,4 @@
 import type { SlackEvent } from "@slack/web-api";
-import {
-  assistantThreadMessage,
-  handleNewAssistantMessage,
-} from "../lib/handle-messages";
-import { waitUntil } from "@vercel/functions";
-import { handleNewAppMention } from "../lib/handle-app-mention";
 import { verifyRequest, getBotId } from "../lib/slack-utils";
 
 export async function POST(request: Request) {
@@ -21,27 +15,9 @@ export async function POST(request: Request) {
 
   try {
     const botUserId = await getBotId();
-
     const event = payload.event as SlackEvent;
 
-    if (event.type === "app_mention") {
-      waitUntil(handleNewAppMention(event, botUserId));
-    }
-
-    if (event.type === "assistant_thread_started") {
-      waitUntil(assistantThreadMessage(event));
-    }
-
-    if (
-      event.type === "message" &&
-      !event.subtype &&
-      event.channel_type === "im" &&
-      !event.bot_id &&
-      !event.bot_profile &&
-      event.bot_id !== botUserId
-    ) {
-      waitUntil(handleNewAssistantMessage(event, botUserId));
-    }
+    // TODO: Implement event handling logic here
 
     return new Response("Success!", { status: 200 });
   } catch (error) {
